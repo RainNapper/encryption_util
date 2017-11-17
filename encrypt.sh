1ed4800f6b0fb4e0826a062d4703ba7708b4369d
Mansfield <mansfield.r.mark@gmail.com>
2017-11-16 21:49:54 -0800

    actually updated stuff

--- encrypt.sh
+++ encrypt.sh
@@ -1,4 +1,5 @@
-if [ -z "$1" ] ; then
+if [ -z '$1' ]
+then
   echo "No argument supplied. Provide path to file to be encrypted."
   echo "Example usage: ./encrypt.sh ./path/to_file.txt"
   exit 1
@@ -15,6 +16,7 @@ fi
 mkdir $OUT_DIR
 openssl rand -base64 128 -out ./$OUT_DIR/otp.key
 openssl enc -aes-256-cbc -salt -in ./$1 -out ./$OUT_DIR/$1.enc -pass file:./$OUT_DIR/otp.key
-echo "openssl enc -d -aes-256-cbc -in ./$1.enc -out ./$1 -pass file:./otp.key" > ./$OUT_DIR/decrypt.sh
-echo $'Usage:\nbash ./decrypt.sh' > ./$OUT_DIR/README
-echo "Successfully encrypted file. Output in ${OUT_DIR}"
+echo "openssl enc -d -aes-256-cbc -in ./$1.enc -out ./$1 -pass file:./otp.key" >> ./$OUT_DIR/decrypt.sh
+echo "echo \"Decrypted file located: ./$1\"" >> ./$OUT_DIR/decrypt.sh
+echo $'Usage:\nbash ./decrypt.sh' >> ./$OUT_DIR/README
+echo "Successfully encrypted file. Output in ./$OUT_DIR"
